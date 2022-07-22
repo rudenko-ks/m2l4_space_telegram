5f15c82766f8908378f0c04b48276e5c5745e154
Kirill Rudenko <cs1@list.ru>
2022-07-22 11:19:24 +0300

    feat: добавил создание папки "images"
    и скачивание картинок в неё

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import requests
+from pathlib import Path
 
 
 def main():
+    img_path = "images/"
+    Path(img_path).mkdir(parents=True, exist_ok=True)
+    
     filename = "satellite.jpeg"
     url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
 
     response = requests.get(url)
     response.raise_for_status()
 
-    with open(filename, "wb") as file:
+    with open(img_path + filename, "wb") as file:
         file.write(response.content)
-
 
 if __name__ == "__main__":
     main()

fdec6d4dbb78955ab54e2600ec00eabe4bd8002d
Kirill Rudenko <cs1@list.ru>
2022-07-22 16:43:59 +0300

    refactor: загрузка фоток запуска SpaceX как функцию

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
-from socket import NI_NAMEREQD
 import requests
 from pathlib import Path
 
 SPCX_LAUNCH_ID = "5eb87ce3ffd86e000604b336"
 
-def download_image(url: str, path: str) -> None:
-    response = requests.get(url)
-    response.raise_for_status()
 
-    filename = "hubble.jpeg"
-    with open(path + filename, "wb") as file:
-        file.write(response.content)
+def download_images(images_urls: list, path: str) -> None:
+    Path(path).mkdir(parents=True, exist_ok=True)
+
+    for img_number, image_url in enumerate(images_urls):
+        response = requests.get(image_url)
+        response.raise_for_status()
+        
+        filename = f"spacex_{img_number}.jpeg"
+        with open(path + filename, "wb") as file:
+            file.write(response.content)
 
 
-def get_spcx_launch_images(launch_id: str) -> None:
-    url = f"https://api.spacexdata.com/v5/launches/{launch_id}"
-    response = requests.get(url)
+def fetch_spacex_last_launch(launch_id: str) -> None:
+    img_folder_path = "./images/"
+    spacex_api_url = f"https://api.spacexdata.com/v5/launches/{launch_id}"
+    
+    response = requests.get(spacex_api_url)
     response.raise_for_status()
     launch = response.json()
-    return launch["links"]["flickr"]["original"]
+    
+    launch_images_urls = launch["links"]["flickr"]["original"]
+    download_images(launch_images_urls, img_folder_path)
 
 
 def main():
-    img_path = "images/"
-    Path(img_path).mkdir(parents=True, exist_ok=True)
-
-    url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
-    download_image(url, img_path)
-    
-    spcx_launch_images = get_spcx_launch_images(SPCX_LAUNCH_ID)
-    print(spcx_launch_images)
+    fetch_spacex_last_launch(SPCX_LAUNCH_ID)
 
 if __name__ == "__main__":
     main()

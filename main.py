e37330558bb98b192b0eebd763b2a81127e69d5f
Kirill Rudenko <cs1@list.ru>
2022-07-22 11:56:59 +0300

    refactor: оформил скачивание картинки как функцию

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ import requests
 from pathlib import Path
 
 
-def main():
-    img_path = "images/"
-    Path(img_path).mkdir(parents=True, exist_ok=True)
-    
-    filename = "satellite.jpeg"
-    url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
-
+def download_image(url: str, path: str) -> None:
     response = requests.get(url)
     response.raise_for_status()
 
-    with open(img_path + filename, "wb") as file:
+    filename = "hubble.jpeg"
+    with open(path + filename, "wb") as file:
         file.write(response.content)
+
+
+def main():
+    img_path = "images/"
+    Path(img_path).mkdir(parents=True, exist_ok=True)
+
+    url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+    download_image(url, img_path)
 
 if __name__ == "__main__":
     main()
